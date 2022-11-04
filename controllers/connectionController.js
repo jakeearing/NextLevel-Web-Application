9c762302f5cb7aba8f7eb820d1c7ca2fb4c3e209
Jake Earing <jakeearing7@gmail.com>
2022-11-04 01:53:21 -0400

    Model/Controller updated to work with Mongoose/mongodb
    Model and controller were completely overhauled to work with mongoose/mongoDB instead of a simple javascript object.

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -1,64 +1,114 @@
 const model = require('../models/connection');
 
-exports.index = (req, res) => {
-    let events = model.find();
-    res.render('./connections/index', { events });
+exports.index = (req, res, next) => {
+    model.find()
+    .then(events=>res.render('./connections/index', {events}))
+    .catch(err=>next(err));  
 };
 
 exports.new = (req, res) => {
     res.render('./connections/new');
 };
 
-exports.create = (req, res) => {
-    let event = req.body;
-    model.save(event);
-    res.redirect('/connections');
+exports.create = (req, res, next) => {
+    let event = new model(req.body);
+    event.save()
+    .then((event)=>{
+        console.log(event);
+        res.redirect('/connections');
+    })
+    .catch(err=>{
+        if (err.name === 'ValidationError')
+            err.status = 400;
+        next(err);
+    });
 };
 
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    let event = model.findById(id);
-    if (event) {
-        res.render('./connections/show', { event });
-    } else {
-        let err = new Error('Cannot find an event with id ' + id);
-        err.status = 404;
-        next(err);
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id');
+        err.status = 400;
+        return next(err);  
     }
+    model.findById(id)
+    .then(event=>{
+        if(event){
+            return res.render('./connections/show', { event });
+        } else {
+            let err = new Error('Cannot find an event with id ' + id);
+                err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err));
 };
 
 exports.edit = (req, res, next) => {
     let id = req.params.id;
-    let event = model.findById(id);
-    if (event) {
-        res.render('./connections/edit', { event });
-    } else {
-        let err = new Error('Cannot find an event with id ' + id);
-        err.status = 404;
-        next(err);
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id');
+        err.status = 400;
+        return next(err);  
     }
+    model.findById(id)
+    .then(event=>{
+        if(event){
+            return res.render('./connections/edit', {event});
+        } else {
+            let err = new Error('Cannot find an event with id ' + id);
+                err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err));
 };
 
 exports.update = (req, res, next) => {
     let event = req.body;
     let id = req.params.id;
 
-    if (model.updateById(id, event)) {
-        res.redirect('/connections/' + id);
-    } else {
-        let err = new Error('Cannot find an event with id ' + id);
-        err.status = 404;
-        next(err);
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id');
+        err.status = 400;
+        return next(err);  
     }
+
+    model.findByIdAndUpdate(id, event, {useFindAndModify: false, runValidators: true})
+    .then(event=>{
+        if(event) {
+            res.redirect('/connections/' + id);
+        } else {
+            let err = new Error('Cannot find an event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=> {
+        if (err.name === 'ValidationError')
+            err.status = 400;
+        next(err);
+    });
 };
 
 exports.delete = (req, res, next) => {
     let id = req.params.id;
-    if (model.deleteById(id)) {
-        res.redirect('/connections');
-    } else {
-        let err = new Error('Cannot find an event with id ' + id);
-        err.status = 404;
-        next(err);
+
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id');
+        err.status = 400;
+        return next(err);  
     }
+
+    model.findByIdAndDelete(id, {useFindAndModify: false})
+    .then(event =>{
+        if(event) {
+            res.redirect('/connections');
+        } else {
+            let err = new Error('Cannot find an event with id ' + id);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err=>next(err));
 };
--- models/connection.js
+++ models/connection.js
@@ -1,116 +1,19 @@
-const { DateTime } = require("luxon");
-const { v4: uuidv4 } = require('uuid');
-const events = [
-    {
-        id: '1',
-        topic: 'Internship',
-        title: 'Connect with the TIAA recruiting team',
-        description: 'Join the TIAA recruitment team for a short presention.',
-        host: 'Tom Thomas',
-        date: DateTime.local(2022, 10, 22).toLocaleString(Date),
-        start: '1:00 PM',
-        end: '1:30 PM',
-        location: 'Woodward Hall Room 261',
-        image: '../images/tiaa.png'
-    },
-    {
-        id: '2',
-        topic: 'Internship',
-        title: 'Meet with a Bank of America Technical Recruiter',
-        description: 'Join BofA Technical Recruiter Kathy Kate for a Q/A about working at Bank of America',
-        host: 'Kathy Kate',
-        date: DateTime.local(2022, 10, 21, 18, 0).toLocaleString(Date),
-        start: '12:00 PM',
-        end: '1:00 PM',
-        location: 'Woodward Hall Room 261',
-        image: '../images/bofa.png'
-    },
-    {
-        id: '3',
-        topic: 'Internship',
-        title: 'Meet with a UNCC Careers Counselor',
-        description: 'Meet with a UNCC Careers Counselor with information on how to make your resume stand out.',
-        host: 'James Jameson',
-        date: DateTime.local(2022, 10, 21, 18, 0).toLocaleString(Date),
-        start: '3:00 PM',
-        end: '4:00 PM',
-        location: 'Woodward Hall Room 140',
-        image: '../images/uncc.png'
-    },
-    {
-        id: '4',
-        topic: 'Tutoring',
-        title: 'CCI Tutoring Q & A Live Session',
-        description: 'Join the CCI Tutoring Department for a Q & A',
-        host: 'Jake Earing',
-        date: DateTime.local(2022, 10, 24, 20, 0).toLocaleString(Date),
-        start: '2:30 PM',
-        end: '3:30 PM',
-        location: 'Woodward Hall Room 250',
-        image: '../images/CCI.png'
-    },
-    {
-        id: '5',
-        topic: 'Tutoring',
-        title: 'Dr. Harini Ramaprasad CCI Tutoring Hiring Event',
-        description: 'Interested in joining the CCI Tutoring Team? We will answer all questions and speak to possible future applicants',
-        host: 'Dr. Harini Ramaprasad',
-        date: DateTime.local(2022, 10, 24, 14, 0).toLocaleString(Date),
-        start: '1:45 PM',
-        end: '2:30 PM',
-        location: 'Woodward Hall Room 141',
-        image: '../images/CCI.png'
-    },
-    {
-        id: '6',
-        topic: 'Tutoring',
-        title: 'CCI Tutoring Final Exam Preparation',
-        description: 'Multiple tutors from the CCI Tutoring Department will be available to help all CCI students prepare for their final exams and projects.',
-        host: 'Jake Earing',
-        date: DateTime.local(2022, 10, 24, 20, 0).toLocaleString(Date),
-        start: '5:00 PM',
-        end: '6:00 PM',
-        location: 'Woodward Hall Room 250',
-        image: '../images/CCI.png'
-    }
-];
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-exports.find = () => events;
+const connectionSchema = new Schema({
+    topic: {type: String, required: [true, 'topic is required']},
+    title: {type: String, required: [true, 'title is required']},
+    description: {type: String, required: [true, 'description is required'], 
+    minLength: [10, 'The description must be at least 10 characters']},
+    host: {type: String, required: [true, 'host is required']},
+    date: {type: Date, required: [true, 'Date is required']},
+    start: {type: String, required: [true, 'start time is required']},
+    end: {type: String, required: [true, 'end time is required']},
+    location: {type: String, required: [true, 'location is required']},
+    image: {type: String, required: [true, 'Image is required']},
+});
 
-exports.findById = id => events.find(event=>event.id === id);
+//Collection name is connections in the database
+module.exports = mongoose.model('Connection', connectionSchema);
 
-exports.save = event => {
-    event.id = uuidv4();
-    //Add placeholder image to event
-    event.image = '../images/placeholder.png';
-    events.push(event);
-};
-
-exports.updateById = (id,newEvent) => {
-    let event = events.find(event=>event.id === id);
-    if (event){
-    event.host = newEvent.host;
-    event.topic = newEvent.topic;
-    event.title = newEvent.title;
-    event.description = newEvent.description;
-    event.date = newEvent.date;
-    event.start = newEvent.start;
-    event.end = newEvent.end;
-    event.location = newEvent.location;
-    //Add placeholder image to event
-    event.image = '../images/placeholder.png';
-    return true;
-    } else {
-        return false;
-    }
-};
-
-exports.deleteById = id => {
-    let index = events.findIndex(event => event.id === id);
-    if(index !== -1) {
-        events.splice(index, 1);
-        return true;
-    } else {
-        return false;
-    }
-}
