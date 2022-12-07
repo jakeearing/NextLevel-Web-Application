913977cbde76229c98777149627c450faf449707
Jake Earing <jakeearing7@gmail.com>
2022-12-07 01:06:00 -0500

    app.js updated for user functionality
    app.js will now support all needed user functionality.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 const connectionRoutes = require('./routes/connectionRoutes');
 const mainRoutes = require('./routes/mainRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -22,6 +26,25 @@ mongoose.connect(url)
 })
 .catch(err=>console.log(err.message));
 
+app.use(
+    session({
+        secret: "8df65das56cvbj0sd",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/NBAD'}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+
+//mount middlware
+app.use((req, res, next) => {
+    res.locals.user = req.session.user||null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
+
 //middleware functions
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
@@ -31,6 +54,8 @@ app.use(methodOverride('_method'));
 app.use('/', mainRoutes);
 
 app.use('/connections', connectionRoutes);
+
+app.use('/users', userRoutes);
 
 //error routes
 app.use((req, res, next) => {
