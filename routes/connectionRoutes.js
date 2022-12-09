4cfd65bdf8fbf3c37da882655fd4cd97205027d6
Jake Earing <jakeearing7@gmail.com>
2022-12-08 20:28:39 -0500

    ConnectionRoutes bugfix
    ValidateResult validator was moved before validateConnection, fixing the connectionRoutes bug.

--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -9,13 +9,13 @@ router.get('/', controller.index);
 
 router.get('/new', isLoggedIn, controller.new);
 
-router.post('/', isLoggedIn, validateConnection, validateResult, controller.create);
+router.post('/', isLoggedIn, validateResult, validateConnection, controller.create);
 
 router.get('/:id', validateId, controller.show);
 
 router.get('/:id/edit', validateId, isLoggedIn, isHost, controller.edit);
 
-router.put('/:id', validateId, isLoggedIn, isHost, validateConnection, validateResult, controller.update);
+router.put('/:id', validateId, isLoggedIn, isHost, validateResult, validateConnection, controller.update);
 
 router.delete('/:id', validateId, isLoggedIn, isHost, controller.delete);
 
