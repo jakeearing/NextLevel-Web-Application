3b65aa110ca69a68e35181868099e30b0c83dc86
Jake Earing <jakeearing7@gmail.com>
2022-12-09 22:39:41 -0500

    mainController bug fix
    fixed a bug on the mainController where the index export was rendering the wrong page.

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,6 @@
 exports.index = (req, res, next)=>{
     let userName = req.session.userName;
-    res.render('./main/about', {userName});
+    res.render('./main/index', {userName});
 };
 
 exports.about = (req, res) => {
